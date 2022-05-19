51e4a6f65ec79d20b66695226a88fc49782ffad1
mateonunez <mateonunez95@gmail.com>
2022-05-19 17:31:32 +0200

    chore(tailwind): add accent colors

--- frontend/react/authentication/tailwind.config.js
+++ frontend/react/authentication/tailwind.config.js
@@ -7,6 +7,18 @@ module.exports = {
         'gradient-y': 'gradient-y 5s ease infinite',
         'gradient-xy': 'gradient-xy 5s ease infinite'
       },
+      colors: {
+        'accent-0': 'var(--accent-0)',
+        'accent-1': 'var(--accent-1)',
+        'accent-2': 'var(--accent-2)',
+        'accent-3': 'var(--accent-3)',
+        'accent-4': 'var(--accent-4)',
+        'accent-5': 'var(--accent-5)',
+        'accent-6': 'var(--accent-6)',
+        'accent-7': 'var(--accent-7)',
+        'accent-8': 'var(--accent-8)',
+        'accent-9': 'var(--accent-9)'
+      },
       fontFamily: {
         incompleeta: ['Incompleeta', 'sans-serif']
       },
