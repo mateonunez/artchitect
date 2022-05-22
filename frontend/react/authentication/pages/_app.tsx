96d1c4063b4f9d1daade8b3e72d58ddf5bdf1a10
mateonunez <mateonunez95@gmail.com>
2022-05-22 14:49:38 +0200

    chore(App): implement AuthContext

--- frontend/react/authentication/pages/_app.tsx
+++ frontend/react/authentication/pages/_app.tsx
@@ -1,15 +1,21 @@
 import { Head } from 'components/common';
+import { AuthContext } from 'lib/contexts';
+import { useProvider } from 'lib/hooks/auth';
 import type { AppProps } from 'next/app';
 import React from 'react';
 import 'styles/global.css';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const authProvider = useProvider() as any;
+
   return (
     <>
       <React.StrictMode>
-        <Head />
+        <AuthContext.Provider value={authProvider}>
+          <Head />
 
-        <Component {...pageProps} />
+          <Component {...pageProps} />
+        </AuthContext.Provider>
       </React.StrictMode>
     </>
   );
