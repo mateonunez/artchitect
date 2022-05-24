f60935f3091647645437f5bb42db46d344e86355
mateonunez <mateonunez95@gmail.com>
2022-05-24 14:28:57 +0200

    feat(Balancer): create users logged in API

--- backend/javascript/balancer/src/index.js
+++ backend/javascript/balancer/src/index.js
@@ -1,16 +1,33 @@
-const express = require('express');
-const dotenv = require('dotenv');
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import express from 'express';
 
-const app = express();
 dotenv.config();
 
 const { BALANCER_HOST, BALANCER_PORT } = process.env;
 
-app.get('/', (req, res) => {
-  res.send(`Hello guest, I'm a Balancer running on the port: ${BALANCER_PORT}!`);
-  console.log('Welcome to the JS Balancer. Time: ' + Date.now());
-});
+const server = () => {
+  const app = express();
 
-app.listen(BALANCER_PORT, BALANCER_HOST, () => {
-  console.log(`Starting Balancer at ${BALANCER_HOST}:${BALANCER_PORT}`);
-});
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.get('/users/logged-in', (req, res) => {
+    console.log('[ balancer (JS) ⚖️ ] Called API: /users/logged-in');
+    res.send({
+      loggedIn: true,
+      user: {
+        id: '123',
+        name: 'John Doe',
+        email: 'john@doe.com'
+      },
+      appId: 'javascript-balancer'
+    });
+  });
+
+  app.listen(BALANCER_PORT, BALANCER_HOST, () => {
+    console.log(`[ balancer (JS) ⚖️ ] Server is listening on ${BALANCER_HOST}:${BALANCER_PORT}`);
+  });
+};
+
+server();
--- backend/rust/balancer/src/main.rs
+++ backend/rust/balancer/src/main.rs
@@ -1,16 +1,42 @@
 extern crate actix_web;
 
 use actix_web::{web, App, HttpServer, Responder};
+use serde::Serialize;
 use log::{debug};
 use std::{env, io};
 
+#[derive(Serialize)]
+struct User {
+  id: String,
+  name: String,
+  email: String,
+}
+
+#[derive(Serialize)]
+struct UserLoggedIn {
+  loggedIn: bool,
+  user: User,
+  appId: String,
+}
+
 const PORT: i32 = 5500;
 
-async fn say_hi() -> impl Responder {
-  let message: String = format!("Hello guest, I'm a Balancer running on the port: {}!", PORT);
+async fn user_logged_in() -> impl Responder {
+  let user = UserLoggedIn {
+    loggedIn: true,
+    user: User {
+      id: "1".to_string(),
+      name: "John Doe".to_string(),
+      email: "john@doe.com".to_string(),
+    },
+    appId: "rust-balancer".to_string(),
+  };
+
+  let message: String = format!("[ balancer (RS) ⚖️ ] Called API: /users/logged-in");
+
   debug!("{}", message);
 
-  message
+  return web::Json(user)
 }
 
 #[actix_rt::main]
@@ -18,7 +44,14 @@ async fn main() -> io::Result<()> {
   env::set_var("RUST_LOG", "debug");
   env_logger::init();
 
-  HttpServer::new(|| App::new().route("/", web::get().to(say_hi)))
+  let message: String = format!("[ balancer (RS) ⚖️ ] Server is listening on {}!", PORT);
+
+  debug!("{}", message);
+
+  HttpServer::new(|| {
+    App::new()
+      .route("/users/logged-in", web::get().to(user_logged_in))
+  })
     .bind(("architect_backend_rust_balancer", 5500))?
     .run()
     .await
