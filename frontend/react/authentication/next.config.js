65e24d0773ceaf404e4bfc8b0b86aa3dcd59752f
mateonunez <mateonunez95@gmail.com>
2022-05-25 09:52:49 +0200

    chore(Auth): add basePath

--- frontend/react/authentication/next.config.js
+++ frontend/react/authentication/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  basePath: '/auth',
   webpackDevMiddleware: config => {
     config.watchOptions = {
       poll: 1000,
