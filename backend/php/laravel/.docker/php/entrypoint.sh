7f653d27a569a24be3abe85d43677d45720e352c
mateonunez <mateonunez95@gmail.com>
2022-06-20 16:51:58 +0200

    chore(Archivel): remove PHP entrypoint and nginx

--- backend/php/laravel/.docker/php/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-envsubst '${DB_HOST},${DB_PORT},${DB_DATABASE},${DB_USERNAME},${DB_PASSWORD},${RABBITMQ_HOST}${RABBITMQ_PORT},${RABBITMQ_USER}${RABBITMQ_PASS}' < /usr/src/architect/backend/php/laravel/.env
-
-exec cd /usr/src/architect/backend/php/laravel && composer install && php artisan migrate:fresh --seed && php artisan passport:install
-exec chown -R $USER:www-data . && find . -type f -exec chmod 664 {} \; && find . -type d -exec chmod 775 {} \; && chgrp -R www-data storage bootstrap/cache && chmod -R ug+rwx storage bootstrap/cache
--- nginx/laravel/entrypoint.sh
+++ nginx/laravel/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-envsubst '$NGINX_ROOT $NGINX_PORT $ARCHIVEL_HOST $ARCHIVEL_PORT' < /etc/nginx/laravel.conf > /etc/nginx/conf.d/default.conf
+envsubst '${NGINX_ROOT},${NGINX_PORT},${ARCHIVEL_HOST},${ARCHIVEL_PORT}' < /etc/nginx/laravel.conf > /etc/nginx/conf.d/default.conf
 
 exec nginx -g "daemon off;"
