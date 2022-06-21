1c6d1a748fc8a2b69c44031cb0d24d5dea70fd19
mateonunez <mateonunez95@gmail.com>
2022-06-21 11:23:58 +0200

    chore(*): add missing env variables

--- nginx/balancer/entrypoint.sh
+++ nginx/balancer/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-envsubst '$NGINX_FPM_HOST' < /etc/nginx/balancer.tmpl > /etc/nginx/conf.d/default.conf
+envsubst '${NGINX_HOST},${NGINX_PORT},${BALANCER_RUST_PORT},${BALANCER_RUST_HOST},${BALANCER_JAVASCRIPT_PORT},${BALANCER_JAVASCRIPT_HOST}' < /etc/nginx/balancer.tmpl > /etc/nginx/conf.d/default.conf
+
 exec nginx -g "daemon off;"
