d8915ac96b8d4abaf9f6a30dad9374703de63f00
mateonunez <mateonunez95@gmail.com>
2022-06-24 10:53:44 +0200

    feat(Mailman): implement Fastify and create simple route

--- backend/typescript/mailman/src/index.ts
+++ backend/typescript/mailman/src/index.ts
@@ -1,18 +1,41 @@
+import { fastify } from 'fastify';
+import pino from 'pino';
 import { sendEmail } from './lib/mailgun';
+/**
+ * Declaring routes
+ */
+const server = fastify({
+  logger: pino({ level: 'info' })
+});
 
 const Mailman = async () => {
   console.log('Mailman is running');
 
-  await sendEmail({
-    to: 'mateonunez95@gmail.com',
-    subject: 'Test',
-    props: {
-      title: 'Test',
-      content: 'Test'
-    }
-  }).catch(error => {
-    console.error(error);
+  server.post('/send-email', async (request, response) => {
+    server.log.info('Received a request to send an email');
+
+    await sendEmail({
+      to: 'mateonunez95@gmail.com',
+      subject: 'Test',
+      props: {
+        title: 'Test',
+        content: 'Test'
+      }
+    }).catch(error => {
+      console.error(error);
+    });
+
+    response.send({
+      message: 'Email sent'
+    });
   });
+
+  try {
+    await server.listen(3000);
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
 };
 
 Mailman();
