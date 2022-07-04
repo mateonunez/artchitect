c6f82ce9087f0ee327519eff8c259ded0c93824e
mateonunez <mateonunez95@gmail.com>
2022-07-04 14:28:28 +0200

    chore(Homepage): change Next config

--- frontend/archifront/homepage/next.config.js
+++ frontend/archifront/homepage/next.config.js
@@ -4,16 +4,14 @@ module.exports = {
   env: {
     BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000',
     HOST: process.env.NEXT_PUBLIC_HOST || 'archifront_nginx',
-    KONG_GATEWAY_ENDPOINT:
-      process.env.NEXT_PUBLIC_KONG_GATEWAY_ENDPOINT || 'http://architect_nginx_laravel:8000',
+    KONG_GATEWAY_ENDPOINT: process.env.NEXT_PUBLIC_KONG_GATEWAY_ENDPOINT || 'http://kong:8000',
     ARCHIFRONT_ENDPOINT:
-      process.env.NEXT_PUBLIC_ARCHIFRONT_ENDPOINT || 'http://architect_nginx_archifront:80',
+      process.env.NEXT_PUBLIC_ARCHIFRONT_ENDPOINT || 'http://archifront_nginx:80',
     ARCHIFRONT_AUTHENTICATION_ENDPOINT:
       process.env.NEXT_PUBLIC_ARCHIFRONT_AUTHENTICATION_ENDPOINT ||
-      'http://architect_frontend_archifront_authentication:3010',
+      'http://archifront_authentication:3010',
     ARCHIFRONT_HOMEPAGE_ENDPOINT:
-      process.env.NEXT_PUBLIC_ARCHIFRONT_HOMEPAGE_ENDPOINT ||
-      'http://architect_frontend_archifront_homepage:3030'
+      process.env.NEXT_PUBLIC_ARCHIFRONT_HOMEPAGE_ENDPOINT || 'http://archifront_homepage:3030'
   },
   webpackDevMiddleware: config => {
     config.watchOptions = {
