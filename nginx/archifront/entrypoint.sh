490e28b9153f3be12ef1bca2eda04f148d1a020f
mateonunez <mateonunez95@gmail.com>
2022-07-04 12:20:43 +0200

    chore(Archifront): rename env variable to NGINX_HOST

--- nginx/archifront/entrypoint.sh
+++ nginx/archifront/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-envsubst '$NGINX_FPM_HOST,$ARCHIFRONT_HOMEPAGE_HOST,$ARCHIFRONT_AUTHENTICATION_HOST' < /etc/nginx/archifront.tmpl > /etc/nginx/conf.d/default.conf
+envsubst '$NGINX_HOST,$ARCHIFRONT_HOMEPAGE_HOST,$ARCHIFRONT_AUTHENTICATION_HOST' < /etc/nginx/archifront.tmpl > /etc/nginx/conf.d/default.conf
 
 exec nginx -g "daemon off;"
