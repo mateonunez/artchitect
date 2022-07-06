8f6695a59fa80917c46d7aa056fcb09dcb78a41b
mateonunez <mateonunez95@gmail.com>
2022-07-06 10:35:32 +0200

    chore(Archivel): format lighthouse config

--- backend/php/laravel/config/lighthouse.php
+++ backend/php/laravel/config/lighthouse.php
@@ -248,7 +248,10 @@ return [
     |
     */
 
-    'debug' => env('LIGHTHOUSE_DEBUG', \GraphQL\Error\DebugFlag::INCLUDE_DEBUG_MESSAGE | \GraphQL\Error\DebugFlag::INCLUDE_TRACE),
+    'debug' => env(
+        'LIGHTHOUSE_DEBUG',
+        \GraphQL\Error\DebugFlag::INCLUDE_DEBUG_MESSAGE | \GraphQL\Error\DebugFlag::INCLUDE_TRACE
+    ),
 
     /*
     |--------------------------------------------------------------------------
